4cc185f18b1c07390ba2774927f1254e066dbc19
Matthew McKney <mrmckney@gmail.com>
2021-07-28 11:26:28 -0400

    added import

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const express = require('express')
 const cors = require('cors');
+const {createUser} = require('./src/users')
 
 const app = express()
 app.use(cors)
